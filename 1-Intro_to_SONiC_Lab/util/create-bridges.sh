14f6813f440425dbedae21fbd2d63713c8240e06
brmcdoug <brmcdoug@cisco.com>
2023-08-09 17:33:24 -0700

    ip link

--- 1-Intro_to_SONiC_Lab/util/create-bridges.sh
+++ 1-Intro_to_SONiC_Lab/util/create-bridges.sh
@@ -6,8 +6,9 @@ sudo brctl addbr r1e16-r4e0
 sudo brctl addbr r1e24-r4e8
 sudo brctl addbr r1e32-host1
 sudo brctl addbr r1e40-host2
-sudo brctl addbr r1e64-host1
-sudo brctl addbr r1e72-host2
+
+sudo brctl addbr leaf01e32-host1
+sudo brctl addbr leaf02e32-host2
 
 sudo brctl addbr r2e0-r3e16
 sudo brctl addbr r2e8-r3e24
@@ -15,8 +16,6 @@ sudo brctl addbr r2e16-r4e16
 sudo brctl addbr r2e24-r4e24
 sudo brctl addbr r2e32-host3
 sudo brctl addbr r2e40-host4
-sudo brctl addbr r2e64-host3
-sudo brctl addbr r2e72-host4
 
 sudo ip link set r1e0-r3e0 up
 sudo ip link set r1e8-r3e8 up
@@ -25,9 +24,6 @@ sudo ip link set r1e24-r4e8 up
 sudo ip link set r1e32-host1 up
 sudo ip link set r1e40-host2 up
 
-sudo ip link set r1e64-host1 up
-sudo ip link set r1e72-host2 up
-
 sudo ip link set r2e0-r3e16 up
 sudo ip link set r2e8-r3e24 up
 sudo ip link set r2e16-r4e16 up
@@ -35,5 +31,5 @@ sudo ip link set r2e24-r4e24 up
 sudo ip link set r2e32-host3 up
 sudo ip link set r2e40-host4 up
 
-sudo ip link set r2e64-host3 up
-sudo ip link set r2e72-host4 up
+sudo ip link set leaf01e32-host1 up
+sudo ip link set leaf02e32-host2 up
