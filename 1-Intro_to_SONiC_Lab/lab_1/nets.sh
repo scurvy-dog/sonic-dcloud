877c69519b3ab32ea461f27edb3b5de6d7a968e2
brmcdoug <brmcdoug@cisco.com>
2023-08-14 18:54:00 -0700

    nets script

---
+++ 1-Intro_to_SONiC_Lab/lab_1/nets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo ip link set eth1 up
+sudo ip link set eth2 up
+
+sudo brctl addbr leaf01e32-host1
+sudo brctl addbr leaf02e32-host2
+
+sudo ip link set leaf01e32-host1 up
+sudo ip link set leaf02e32-host2 up
+
+sudo brctl addif leaf01e32-host1 eth1
+sudo brctl addif leaf02e32-host2 eth2
+
+sudo ip addr add 10.1.2.3/32 dev leaf01e32-host1
+sudo ip addr add 10.1.3.3/32 dev leaf02e32-host2
