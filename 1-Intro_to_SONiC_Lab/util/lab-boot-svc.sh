7d68f8d490b97421392753dd0a37deea5506e6ae
brmcdoug <brmcdoug@cisco.com>
2023-08-24 15:02:22 -0700

    boot svc

--- 1-Intro_to_SONiC_Lab/util/lab-boot-svc.sh
+++ 1-Intro_to_SONiC_Lab/util/lab-boot-svc.sh
@@ -3,9 +3,8 @@
 date > /home/cisco/util/boot-service.log
 
 # gitpull
-echo "git pull
-
-" >> /home/cisco/util/boot-service.log
+echo "
+git pull" >> /home/cisco/util/boot-service.log
 
 export HOME=/home/cisco
 cd /home/cisco/sonic-dcloud
@@ -14,21 +13,16 @@ git pull >> /home/cisco/util/boot-service.log
 
 # nets
 echo "
-
-create endpoint bridges and routes" >> /home/cisco/util/boot-service.log
+create endpoint bridges and routes, brctl show" >> /home/cisco/util/boot-service.log
 
 sudo brctl addbr leaf01-host1
 sudo brctl addbr leaf02-host2
-
 sudo ip link set leaf01-host1 up
 sudo ip link set leaf02-host2 up
-
 sudo brctl addif leaf01-host1 eth1
 sudo brctl addif leaf02-host2 eth2
-
 sudo ip addr add 10.1.2.3/32 dev leaf01-host1
 sudo ip addr add 10.1.3.3/32 dev leaf02-host2
-
 sudo ip route add 10.1.2.0/24 dev leaf01-host1
 sudo ip route add 10.1.3.0/24 dev leaf02-host2
 
@@ -37,17 +31,16 @@ ip route >> /home/cisco/util/boot-service.log
 
 # Virsh
 echo "
-
 virsh start sonic VMs" >> /home/cisco/util/boot-service.log
 
 virsh start spine01 >> /home/cisco/util/boot-service.log
 virsh start spine02 >> /home/cisco/util/boot-service.log
 
 sleep 5
-
 virsh start leaf01 >> /home/cisco/util/boot-service.log
 virsh start leaf02 >> /home/cisco/util/boot-service.log
 
 sleep 5
-
+echo "
+virsh list --all" >> /home/cisco/util/boot-service.log
 virsh list --all >> /home/cisco/util/boot-service.log
