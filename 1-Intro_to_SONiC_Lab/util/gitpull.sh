fcdccf087882c4ff21b07ba906d194a14bbe6420
brmcdoug <brmcdoug@cisco.com>
2023-08-24 10:29:52 -0700

    boot svc

--- 1-Intro_to_SONiC_Lab/util/gitpull.sh
+++ 1-Intro_to_SONiC_Lab/util/gitpull.sh
@@ -6,3 +6,16 @@ git config --global --add safe.directory /home/cisco/sonic-dcloud
 git pull
 
 /1-Intro_to_SONiC_Lab/util/nets.sh
+
+date >> /home/cisco/util/boot-service.log
+virsh start spine01 >> /home/cisco/util/boot-service.log
+virsh start spine02 >> /home/cisco/util/boot-service.log
+
+sleep 5
+
+virsh start leaf01 >> /home/cisco/util/boot-service.log
+virsh start leaf02 >> /home/cisco/util/boot-service.log
+
+sleep 5
+
+virsh list --all >> /home/cisco/util/boot-service.log
--- 1-Intro_to_SONiC_Lab/util/nets.sh
+++ 1-Intro_to_SONiC_Lab/util/nets.sh
@@ -17,16 +17,3 @@ sudo ip addr add 10.1.3.3/32 dev leaf02-host2
 
 sudo ip route add 10.1.2.0/24 dev leaf01-host1
 sudo ip route add 10.1.3.0/24 dev leaf02-host2
-
-date >> /home/cisco/util/boot-service.log
-virsh start spine01 >> /home/cisco/util/boot-service.log
-virsh start spine02 >> /home/cisco/util/boot-service.log
-
-sleep 5
-
-virsh start leaf01 >> /home/cisco/util/boot-service.log
-virsh start leaf02 >> /home/cisco/util/boot-service.log
-
-sleep 5
-
-virsh list --all >> /home/cisco/util/boot-service.log
