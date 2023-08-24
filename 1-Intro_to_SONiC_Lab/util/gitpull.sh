f68136c03c2d3d812d2efa4775dc9f678c512f62
brmcdoug <brmcdoug@cisco.com>
2023-08-24 10:48:46 -0700

    boot svc

--- 1-Intro_to_SONiC_Lab/util/gitpull.sh
+++ 1-Intro_to_SONiC_Lab/util/gitpull.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+# gitpull
 export HOME=/home/cisco
 cd /home/cisco/sonic-dcloud
 git config --global --add safe.directory /home/cisco/sonic-dcloud
 git pull
 
-/1-Intro_to_SONiC_Lab/util/nets.sh
+# nets
+#sudo brctl addbr mgt-net
+sudo brctl addbr leaf01-host1
+sudo brctl addbr leaf02-host2
 
+#sudo ip link set mgt-net up
+sudo ip link set leaf01-host1 up
+sudo ip link set leaf02-host2 up
+
+sudo brctl addif leaf01-host1 eth1
+sudo brctl addif leaf02-host2 eth2
+
+sudo ip addr add 10.1.2.3/32 dev leaf01-host1
+sudo ip addr add 10.1.3.3/32 dev leaf02-host2
+
+sudo ip route add 10.1.2.0/24 dev leaf01-host1
+sudo ip route add 10.1.3.0/24 dev leaf02-host2
+
+# Virsh
 date >> /home/cisco/util/boot-service.log
 virsh start spine01 >> /home/cisco/util/boot-service.log
 virsh start spine02 >> /home/cisco/util/boot-service.log
