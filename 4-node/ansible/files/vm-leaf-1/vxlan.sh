3279e118e1db65046cb321dd5e078342d1038264
brmcdoug <brmcdoug@cisco.com>
2023-09-16 18:34:26 -0700

    4-node one host

---
+++ 4-node/ansible/files/vm-leaf-1/vxlan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo clab tools vxlan create --remote 172.10.10.103 --id 10 --link leaf-1-eth1 --mtu 9200
+sudo clab tools vxlan create --remote 172.10.10.103 --id 20 --link leaf-1-eth2 --mtu 9200
+sudo clab tools vxlan create --remote 172.10.10.104 --id 30 --link leaf-1-eth3
+sudo clab tools vxlan create --remote 172.10.10.104 --id 40 --link leaf-1-eth4
