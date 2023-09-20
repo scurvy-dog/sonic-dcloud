58cec6846557f94439ea719a19d8a61b21d66296
brmcdoug <brmcdoug@cisco.com>
2023-09-20 16:03:20 -0700

    pbk

--- 1-Intro_to_SONiC_Lab/ansible/files/vm-leaf-2/vxlan.sh
+++ 1-Intro_to_SONiC_Lab/ansible/files/vm-leaf-2/vxlan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo clab tools vxlan create --remote 172.10.10.104 --id 10 --link leaf-2-eth1
-sudo clab tools vxlan create --remote 172.10.10.104 --id 20 --link leaf-2-eth2
-sudo clab tools vxlan create --remote 172.10.10.103 --id 30 --link leaf-2-eth3
-sudo clab tools vxlan create --remote 172.10.10.103 --id 40 --link leaf-2-eth4
+sudo clab tools vxlan create --remote 172.10.10.204 --id 10 --link leaf-2-eth1
+sudo clab tools vxlan create --remote 172.10.10.204 --id 20 --link leaf-2-eth2
+sudo clab tools vxlan create --remote 172.10.10.203 --id 30 --link leaf-2-eth3
+sudo clab tools vxlan create --remote 172.10.10.203 --id 40 --link leaf-2-eth4
--- 1-Intro_to_SONiC_Lab/ansible/files/vm-spine-1/vxlan.sh
+++ 1-Intro_to_SONiC_Lab/ansible/files/vm-spine-1/vxlan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo clab tools vxlan create --remote 172.10.10.101 --id 10 --link spine-1-eth1 --mtu 9200
-sudo clab tools vxlan create --remote 172.10.10.101 --id 20 --link spine-1-eth2 --mtu 9200
-sudo clab tools vxlan create --remote 172.10.10.102 --id 30 --link spine-1-eth3
-sudo clab tools vxlan create --remote 172.10.10.102 --id 40 --link spine-1-eth4
+sudo clab tools vxlan create --remote 172.10.10.201 --id 10 --link spine-1-eth1 --mtu 9200
+sudo clab tools vxlan create --remote 172.10.10.201 --id 20 --link spine-1-eth2 --mtu 9200
+sudo clab tools vxlan create --remote 172.10.10.202 --id 30 --link spine-1-eth3
+sudo clab tools vxlan create --remote 172.10.10.202 --id 40 --link spine-1-eth4
