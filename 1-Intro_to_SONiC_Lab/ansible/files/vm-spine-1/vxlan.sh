1f2724d880e3af0dfe3ae95d61faeb86a450a5ab
brmcdoug <brmcdoug@cisco.com>
2023-09-21 14:56:21 -0700

    test playbook

--- 1-Intro_to_SONiC_Lab/ansible/files/vm-spine-1/vxlan.sh
+++ 1-Intro_to_SONiC_Lab/ansible/files/vm-spine-1/vxlan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#sudo clab tools vxlan delete
+
 sudo clab tools vxlan create --remote 172.10.10.201 --id 10 --link spine-1-eth1 --mtu 9200
 sudo clab tools vxlan create --remote 172.10.10.201 --id 20 --link spine-1-eth2 --mtu 9200
 sudo clab tools vxlan create --remote 172.10.10.202 --id 30 --link spine-1-eth3
