4a296050fa0c0cdef5f9d95895164ef80dfa47ea
robermur <robermur@cisco.com>
2023-10-04 10:21:53 -0700

    Removed top level directories no longer being used

--- 4-node/ansible/files/vm-leaf-2/vxlan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo clab tools vxlan create --remote 172.10.10.104 --id 10 --link leaf-2-eth1
-sudo clab tools vxlan create --remote 172.10.10.104 --id 20 --link leaf-2-eth2
-sudo clab tools vxlan create --remote 172.10.10.103 --id 30 --link leaf-2-eth3
-sudo clab tools vxlan create --remote 172.10.10.103 --id 40 --link leaf-2-eth4
