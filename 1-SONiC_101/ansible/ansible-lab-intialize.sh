ab39479d117458d64a83d181196dba5b2eedb89f
brmcdoug <brmcdoug@cisco.com>
2023-10-12 16:25:57 -0700

    sh

--- 1-SONiC_101/ansible/ansible-lab-intialize.sh
+++ 1-SONiC_101/ansible/ansible-lab-intialize.sh
@@ -6,3 +6,7 @@ date >> /home/cisco/.startup.log
 whoami >> /home/cisco/.startup.log
 
 ansible-playbook -i /home/cisco/sonic-dcloud/1-SONiC_101/ansible/hosts -e "ansible_user=cisco ansible_ssh_pass=cisco123 ansible_sudo_pass=cisco123" /home/cisco/sonic-dcloud/1-SONiC_101/ansible/dcloud_startup_playbook.yml -v >> /home/cisco/.startup.log
+
+sleep 5
+
+echo "starting ansible health check playbook"
