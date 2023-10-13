13ee84605573a376f7a435e0b017ef9577272dcf
Robert Murphy <robermur@cisco.com>
2023-10-13 07:58:14 -0700

    Update ansible-lab-intialize.sh

--- 1-SONiC_101/ansible/ansible-lab-intialize.sh
+++ 1-SONiC_101/ansible/ansible-lab-intialize.sh
@@ -10,5 +10,4 @@ ansible-playbook -i /home/cisco/sonic-dcloud/1-SONiC_101/ansible/hosts -e "ansib
 sleep 5
 
 # ansible-playbook -e "ansible_user=cisco ansible_ssh_pass=cisco123 ansible_sudo_pass=cisco123" /home/cisco/sonic-dcloud/1-SONiC_101/ansible/sonic_node_health_check.yml -v >> /home/cisco/.startup.log
-
-echo "starting ansible health check playbook"
+# echo "starting ansible health check playbook"
