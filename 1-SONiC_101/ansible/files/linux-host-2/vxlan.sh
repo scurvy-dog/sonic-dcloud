b30d8f1d3d8f32a87be2f71acaad3dd52d8d5d3c
brmcdoug <brmcdoug@cisco.com>
2023-10-13 15:30:59 -0700

    typo

--- 1-SONiC_101/ansible/files/linux-host-2/vxlan.sh
+++ 1-SONiC_101/ansible/files/linux-host-2/vxlan.sh
@@ -2,7 +2,7 @@
 
 # sudo clab tools vxlan delete
 
-sudo clab tools vxlan create --remote 172.10.10.204 --id 10 --link sonic-rtr-leaf-2-eth1
-sudo clab tools vxlan create --remote 172.10.10.204 --id 20 --link sonic-rtr-leaf-2-eth2
-sudo clab tools vxlan create --remote 172.10.10.203 --id 30 --link sonic-rtr-leaf-2-eth3
-sudo clab tools vxlan create --remote 172.10.10.203 --id 40 --link sonic-rtr-leaf-2-eth4
+sudo clab tools vxlan create --remote 172.10.10.204 --id 10 --link leaf-2-eth1
+sudo clab tools vxlan create --remote 172.10.10.204 --id 20 --link leaf-2-eth2
+sudo clab tools vxlan create --remote 172.10.10.203 --id 30 --link leaf-2-eth3
+sudo clab tools vxlan create --remote 172.10.10.203 --id 40 --link leaf-2-eth4
