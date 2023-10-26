2f82310fd214f1d128388f09af2628e2deb3d44a
brmcdoug <brmcdoug@cisco.com>
2023-10-26 08:00:01 -0700

    vxr

--- 1-SONiC_101/vxr/parse.py
+++ 1-SONiC_101/vxr/parse.py
@@ -1,22 +1,22 @@
 import os
 import json
 
-d = os.popen('arp -an | grep virbr0').read().strip()
-print(d)
+# d = os.popen('arp -an | grep virbr0').read().strip()
+# print(d)
 
-# d = """? (192.168.122.82) at 02:62:8b:b6:a3:bd [ether] on virbr0
-# ? (192.168.122.62) at 02:36:0f:98:64:cc [ether] on virbr0
-# ? (192.168.122.157) at 02:2d:41:3a:64:9d [ether] on virbr0
-# ? (192.168.122.122) at 02:3b:1c:53:20:5b [ether] on virbr0"""
+d = """? (192.168.122.82) at 02:62:8b:b6:a3:bd [ether] on virbr0
+? (192.168.122.62) at 02:36:0f:98:64:cc [ether] on virbr0
+? (192.168.122.157) at 02:2d:41:3a:64:9d [ether] on virbr0
+? (192.168.122.122) at 02:3b:1c:53:20:5b [ether] on virbr0"""
 
 dl = d.split("\n")
 
 def Convert(lst):
     res_dct = map(lambda i: (lst[i], lst[i+1]), range(len(lst)-1)[::2])
     return dict(res_dct)
+ml = []
 
 for l in dl:
-    ml = []
     pl = l.split(" ")
     pl = [w.replace('?', 'ip') for w in pl]
     pl = [w.replace('at', 'mac') for w in pl]
@@ -24,12 +24,16 @@ for l in dl:
     pl = ' '.join(pl).replace('(','').split()
     pl = ' '.join(pl).replace(')','').split()
 
+    r = os.popen('grep -r ' + pl[3]).read().strip()
+    print(r)
+
     # Driver code
     lst = pl
-    cl = (Convert(lst))
+    clst = [x.upper() for x in lst]
+    cl = (Convert(clst))
+    #print(cl)
 
-    ml.append(cl)
-    print(ml)
-
+    #r = os.popen('grep -r ' + pl[3]).read().strip()
+    # print(d)  
 
  
