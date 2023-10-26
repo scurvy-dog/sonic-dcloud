1e44480b550055489644aa2439b25dc6b5abd0f1
brmcdoug <brmcdoug@cisco.com>
2023-10-26 09:13:06 -0700

    grep

--- 1-SONiC_101/vxr/parse.py
+++ 1-SONiC_101/vxr/parse.py
@@ -53,6 +53,7 @@ for l in dl:
     corr = os.popen('grep -r ' + up[3] + ' /nobackup/root/pyvxr/p*lcc0lc0/ConfigVector.txt').read().strip()
     
     print("node " + corr[21:29] + " has ip " + up[1])
+    print(corr[21:29], leaf1)
     if corr[21:29] == leaf1:
         print("leaf-1 has ip " + up[1])
     if corr[21:29] == leaf2:
