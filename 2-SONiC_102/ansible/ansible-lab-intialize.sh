563efe0873635ab1756310a3cef905803efb7abc
Robert Murphy <robermur@cisco.com>
2024-01-17 13:27:54 -0800

    Update ansible-lab-intialize.sh

--- 2-SONiC_102/ansible/ansible-lab-intialize.sh
+++ 2-SONiC_102/ansible/ansible-lab-intialize.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# This file is needed to bridge the sudo permission issue with crontab vs ansible
 
 echo "startup script launched at: " > /home/cisco/deploy.log
 
 date >> /home/cisco/deploy.log
 whoami >> /home/cisco/deploy.log
 
-ansible-playbook -i /home/cisco/sonic-dcloud/1-SONiC_101/ansible/hosts \
+ansible-playbook -i /home/cisco/sonic-dcloud/1-SONiC_102/ansible/hosts \
   -e "ansible_user=cisco ansible_ssh_pass=cisco123 ansible_sudo_pass=cisco123" \
-  /home/cisco/sonic-dcloud/1-SONiC_101/ansible/dcloud_startup_playbook.yml -v >> /home/cisco/deploy.log
+  /home/cisco/sonic-dcloud/1-SONiC_102/ansible/dcloud_startup_playbook.yml -v >> /home/cisco/deploy.log
 
 
