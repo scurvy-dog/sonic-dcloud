7df1b69f0e6d95683aa4c1fea00b073b8eba67ce
Robert Murphy <robermur@cisco.com>
2024-01-19 09:10:41 -0800

    Update ansible-lab-intialize.sh

--- 2-SONiC_102/ansible/ansible-lab-intialize.sh
+++ 2-SONiC_102/ansible/ansible-lab-intialize.sh
@@ -6,7 +6,7 @@ echo "startup script launched at: " > /home/cisco/deploy.log
 date >> /home/cisco/deploy.log
 whoami >> /home/cisco/deploy.log
 
-ansible-playbook -i /home/cisco/sonic-dcloud/1-SONiC_102/ansible/hosts \
+ansible-playbook -i /home/cisco/sonic-dcloud/2-SONiC_102/ansible/hosts \
   -e "ansible_user=cisco ansible_ssh_pass=cisco123 ansible_sudo_pass=cisco123" \
   /home/cisco/sonic-dcloud/1-SONiC_102/ansible/dcloud_startup_playbook.yml -v >> /home/cisco/deploy.log
 
